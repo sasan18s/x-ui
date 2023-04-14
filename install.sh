f41f3ccf053dd229885719ab6652161d34dac9fa
KinG <61575481+sasan18s@users.noreply.github.com>
2023-04-14 12:05:04 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -81,7 +81,7 @@ install_base() {
     fi
 }
 
-#This function will be called when user installed xx-ui out of sercurity
+#This function will be called when user installed x-ui out of sercurity
 config_after_install() {
     echo -e "${yellow}出于安全考虑，安装/更新完成后需要强制修改端口与账户密码${plain}"
     read -p "确认是否继续?[y/n]": config_confirm
@@ -93,83 +93,83 @@ config_after_install() {
         read -p "请设置面板访问端口:" config_port
         echo -e "${yellow}您的面板访问端口将设定为:${config_port}${plain}"
         echo -e "${yellow}确认设定,设定中${plain}"
-        /usr/local/2/xx-ui/xx-ui setting -username ${config_account} -password ${config_password}
+        /usr/local/x-ui/x-ui setting -username ${config_account} -password ${config_password}
         echo -e "${yellow}账户密码设定完成${plain}"
-        /usr/local/2/xx-ui/xx-ui setting -port ${config_port}
+        /usr/local/x-ui/x-ui setting -port ${config_port}
         echo -e "${yellow}面板端口设定完成${plain}"
     else
         echo -e "${red}已取消,所有设置项均为默认设置,请及时修改${plain}"
     fi
 }
 
-install_xx-ui() {
-    systemctl stop xx-ui
-    cd /usr/local/2/
+install_x-ui() {
+    systemctl stop x-ui
+    cd /usr/local/2
 
     if [ $# == 0 ]; then
-        last_version=$(curl -Ls "https://api.github.com/repos/vaxilu/xx-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        last_version=$(curl -Ls "https://api.github.com/repos/vaxilu/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$last_version" ]]; then
-            echo -e "${red}检测 xx-ui 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 xx-ui 版本安装${plain}"
+            echo -e "${red}检测 x-ui 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 x-ui 版本安装${plain}"
             exit 1
         fi
-        echo -e "检测到 xx-ui 最新版本：${last_version}，开始安装"
-        wget -N --no-check-certificate -O /usr/local/2/xx-ui-linux-${arch}.tar.gz https://github.com/vaxilu/xx-ui/releases/download/${last_version}/xx-ui-linux-${arch}.tar.gz
+        echo -e "检测到 x-ui 最新版本：${last_version}，开始安装"
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/vaxilu/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 xx-ui 失败，请确保你的服务器能够下载 Github 的文件${plain}"
+            echo -e "${red}下载 x-ui 失败，请确保你的服务器能够下载 Github 的文件${plain}"
             exit 1
         fi
     else
         last_version=$1
-        url="https://github.com/vaxilu/xx-ui/releases/download/${last_version}/xx-ui-linux-${arch}.tar.gz"
-        echo -e "开始安装 xx-ui v$1"
-        wget -N --no-check-certificate -O /usr/local/2/xx-ui-linux-${arch}.tar.gz ${url}
+        url="https://github.com/vaxilu/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
+        echo -e "开始安装 x-ui v$1"
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 xx-ui v$1 失败，请确保此版本存在${plain}"
+            echo -e "${red}下载 x-ui v$1 失败，请确保此版本存在${plain}"
             exit 1
         fi
     fi
 
-    if [[ -e /usr/local/2/xx-ui/ ]]; then
-        rm /usr/local/2/xx-ui/ -rf
+    if [[ -e /usr/local/x-ui/ ]]; then
+        rm /usr/local/x-ui/ -rf
     fi
 
-    tar zxvf xx-ui-linux-${arch}.tar.gz
-    rm xx-ui-linux-${arch}.tar.gz -f
-    cd xx-ui
-    chmod +x xx-ui bin/xray-linux-${arch}
-    cp -f xx-ui.service /etc/systemd/system/
-    wget --no-check-certificate -O /usr/bin/xx-ui https://raw.githubusercontent.com/vaxilu/xx-ui/main/xx-ui.sh
-    chmod +x /usr/local/2/xx-ui/xx-ui.sh
-    chmod +x /usr/bin/xx-ui
+    tar zxvf x-ui-linux-${arch}.tar.gz
+    rm x-ui-linux-${arch}.tar.gz -f
+    cd x-ui
+    chmod +x x-ui bin/xray-linux-${arch}
+    cp -f x-ui.service /etc/systemd/system/
+    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/vaxilu/x-ui/main/x-ui.sh
+    chmod +x /usr/local/x-ui/x-ui.sh
+    chmod +x /usr/bin/x-ui
     config_after_install
     #echo -e "如果是全新安装，默认网页端口为 ${green}54321${plain}，用户名和密码默认都是 ${green}admin${plain}"
     #echo -e "请自行确保此端口没有被其他程序占用，${yellow}并且确保 54321 端口已放行${plain}"
-    #    echo -e "若想将 54321 修改为其它端口，输入 xx-ui 命令进行修改，同样也要确保你修改的端口也是放行的"
+    #    echo -e "若想将 54321 修改为其它端口，输入 x-ui 命令进行修改，同样也要确保你修改的端口也是放行的"
     #echo -e ""
     #echo -e "如果是更新面板，则按你之前的方式访问面板"
     #echo -e ""
     systemctl daemon-reload
-    systemctl enable xx-ui
-    systemctl start xx-ui
-    echo -e "${green}xx-ui v${last_version}${plain} 安装完成，面板已启动，"
+    systemctl enable x-ui
+    systemctl start x-ui
+    echo -e "${green}x-ui v${last_version}${plain} 安装完成，面板已启动，"
     echo -e ""
-    echo -e "xx-ui 管理脚本使用方法: "
+    echo -e "x-ui 管理脚本使用方法: "
     echo -e "----------------------------------------------"
-    echo -e "xx-ui              - 显示管理菜单 (功能更多)"
-    echo -e "xx-ui start        - 启动 xx-ui 面板"
-    echo -e "xx-ui stop         - 停止 xx-ui 面板"
-    echo -e "xx-ui restart      - 重启 xx-ui 面板"
-    echo -e "xx-ui status       - 查看 xx-ui 状态"
-    echo -e "xx-ui enable       - 设置 xx-ui 开机自启"
-    echo -e "xx-ui disable      - 取消 xx-ui 开机自启"
-    echo -e "xx-ui log          - 查看 xx-ui 日志"
-    echo -e "xx-ui v2-ui        - 迁移本机器的 v2-ui 账号数据至 xx-ui"
-    echo -e "xx-ui update       - 更新 xx-ui 面板"
-    echo -e "xx-ui install      - 安装 xx-ui 面板"
-    echo -e "xx-ui uninstall    - 卸载 xx-ui 面板"
+    echo -e "x-ui              - 显示管理菜单 (功能更多)"
+    echo -e "x-ui start        - 启动 x-ui 面板"
+    echo -e "x-ui stop         - 停止 x-ui 面板"
+    echo -e "x-ui restart      - 重启 x-ui 面板"
+    echo -e "x-ui status       - 查看 x-ui 状态"
+    echo -e "x-ui enable       - 设置 x-ui 开机自启"
+    echo -e "x-ui disable      - 取消 x-ui 开机自启"
+    echo -e "x-ui log          - 查看 x-ui 日志"
+    echo -e "x-ui v2-ui        - 迁移本机器的 v2-ui 账号数据至 x-ui"
+    echo -e "x-ui update       - 更新 x-ui 面板"
+    echo -e "x-ui install      - 安装 x-ui 面板"
+    echo -e "x-ui uninstall    - 卸载 x-ui 面板"
     echo -e "----------------------------------------------"
 }
 
 echo -e "${green}开始安装${plain}"
 install_base
-install_xx-ui $1
+install_x-ui $1
